db1bc5e8175c23bb41f536bbd10d19abff05e32e
yakovlievv <yakovlievv25@gmail.com>
2025-05-25 16:50:59 +0300

    initial commit

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- vim.opt.guicursor = ""
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+-- build blink.cmp from source, never download
+vim.g.lazyvim_blink_main = true
