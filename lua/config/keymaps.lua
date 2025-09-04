45591612afd445765fb1401e80fd9503a8933dc9
yakovlievv <yakovlievv25@gmail.com>
2025-09-04 14:46:29 +0300

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,6 +9,22 @@ set("n", "J", "mzJ`z")
 set("n", "<leader>=", ":keepjumps normal! magg=G`azz<CR>")
 
 set("n", "<leader>l", ":Lazy<CR>")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlights text when yanking",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+-- Paste non-linewise text above or below current line, see https://stackoverflow.com/a/1346777/6064933
+keymap.set("n", "<leader>p", "m`o<ESC>p``", { desc = "paste below current line" })
+keymap.set("n", "<leader>P", "m`O<ESC>p``", { desc = "paste above current line" })
+
+keymap.set({ "n", "x" }, "H", "^")
+keymap.set({ "n", "x" }, "L", "g_")
 -- set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 -- set("n", "n", "nzzzv")
 -- set("n", "N", "Nzzzv")
