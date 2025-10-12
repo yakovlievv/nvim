3b27f1c400a0fb244e6abfa50bb691e37fe67ef1
yakovlievv <yakovlievv25@gmail.com>
2025-10-12 10:59:20 +0300

    fixed autocompletion with luasnip

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -5,45 +5,47 @@ return {
 	dependencies = {
 		{
 			"L3MON4D3/LuaSnip",
+			dependencies = { "rafamadriz/friendly-snippets" },
 			version = "v2.*",
-			event = "InsertEnter",
 			build = "make install_jsregexp",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
 		},
-		{ "rafamadriz/friendly-snippets" },
-		event = "InsertEnter",
-		opts_extend = { "sources.default" },
-		version = "1.*",
 	},
-	config = function()
-		require("blink-cmp").setup({
+	opts_extend = { "sources.default" },
+	version = "1.*",
 
+	config = function()
+		require("blink.cmp").setup({
 			keymap = {
 				preset = "none",
 				["<C-d"] = { "show_documentation", "hide_documentation" },
-
 				["<C-e>"] = { "hide" },
 				["<C-y>"] = { "select_and_accept", "show" },
-
 				["<Up>"] = { "select_prev", "fallback" },
 				["<Down>"] = { "select_next", "fallback" },
 				["<C-k>"] = { "select_prev", "fallback_to_mappings" },
 				["<C-j>"] = { "select_next", "fallback_to_mappings" },
-
 				["<C-b>"] = { "scroll_documentation_up", "fallback" },
 				["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
 				["<C-l>"] = { "snippet_forward", "fallback" },
 				["<C-h>"] = { "snippet_backward", "fallback" },
 			},
 
-			appearance = { nerd_font_variant = "mono" },
+			appearance = {
+				nerd_font_variant = "mono",
+			},
 
 			completion = {
 				ghost_text = { enabled = true },
 				documentation = { auto_show = true, auto_show_delay_ms = 10 },
 			},
 
-			snippets = { preset = "default" },
+			snippets = {
+				preset = "luasnip",
+			},
+
 			sources = {
 				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
 				providers = {
