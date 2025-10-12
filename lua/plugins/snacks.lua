c5826e5acf1ec811fb38c2ed481bdb5f875349c7
yakovlievv <yakovlievv25@gmail.com>
2025-10-12 15:32:49 +0300

    snacks major update

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,93 +6,59 @@ return {
 		bigfile = { enabled = true },
 		dashboard = {
 			enabled = true,
-			width = 60,
 			row = nil, -- dashboard position. nil for center
 			col = nil, -- dashboard position. nil for center
 			pane_gap = 4, -- empty columns between vertical panes
-			autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", -- autokey sequence
-			-- These settings are used by some built-in sections
-			preset = {
-				-- Defaults to a picker that supports `fzf-lua`, `telescope.nvim` and `mini.pick`
-				---@type fun(cmd:string, opts:table)|nil
-				pick = nil,
-				-- Used by the `keys` section to show keymaps.
-				-- Set your custom keymaps here.
-				-- When using a function, the `items` argument are the default keymaps.
-				---@type snacks.dashboard.Item[]
-				keys = {
-					{
-						icon = " ",
-						key = "f",
-						desc = "Find File",
-						action = ":lua Snacks.dashboard.pick('files')",
-					},
-					{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-					{
-						icon = " ",
-						key = "g",
-						desc = "Find Text",
-						action = ":lua Snacks.dashboard.pick('live_grep')",
-					},
-					{
-						icon = " ",
-						key = "y",
-						desc = "Open Yazi",
-						action = ":Yazi toggle",
-					},
-					{
-						icon = " ",
-						key = "r",
-						desc = "Recent Files",
-						action = ":lua Snacks.dashboard.pick('oldfiles')",
-					},
-					-- {
-					-- 	icon = " ",
-					-- 	key = "c",
-					-- 	desc = "Config",
-					-- 	action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-					-- },
-					-- { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-					{
-						icon = "󰒲 ",
-						key = "l",
-						desc = "Lazy",
-						action = ":Lazy",
-						enabled = package.loaded.lazy ~= nil,
-					},
-					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+			keys = {
+				{
+					icon = " ",
+					key = "f",
+					desc = "Find File",
+					action = ":lua Snacks.dashboard.pick('files')",
 				},
-				-- Used by the `header` section
-				header = [[
-███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
-██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
-██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+				{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+				{
+					icon = " ",
+					key = "g",
+					desc = "Find Text",
+					action = ":lua Snacks.dashboard.pick('live_grep')",
+				},
+				{
+					icon = " ",
+					key = "y",
+					desc = "Open Yazi",
+					action = ":Yazi toggle",
+				},
+				{
+					icon = " ",
+					key = "r",
+					desc = "Recent Files",
+					action = ":lua Snacks.dashboard.pick('oldfiles')",
+				},
+				-- {
+				-- 	icon = " ",
+				-- 	key = "c",
+				-- 	desc = "Config",
+				-- 	action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+				-- },
+				-- { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+				{
+					icon = "󰒲 ",
+					key = "l",
+					desc = "Lazy",
+					action = ":Lazy",
+					enabled = package.loaded.lazy ~= nil,
+				},
+				{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
 			},
-			-- item field formatters
-			formats = {
-				icon = function(item)
-					return { item.icon or "", width = 2, hl = "icon" }
-				end,
-				footer = { "%s", align = "center" },
-				header = { "%s", align = "center" },
-				file = function(item, ctx)
-					local fname = vim.fn.fnamemodify(item.file, ":~")
-					fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
-					if #fname > ctx.width then
-						local dir = vim.fn.fnamemodify(fname, ":h")
-						local file = vim.fn.fnamemodify(fname, ":t")
-						if dir and file then
-							file = file:sub(-(ctx.width - #dir - 2))
-							fname = dir .. "/…" .. file
-						end
-					end
-					local dir, file = fname:match("^(.*)/(.+)$")
-					return dir and { { dir .. "/", hl = "dir" }, { file, hl = "file" } } or { { fname, hl = "file" } }
-				end,
-			},
+			-- Used by the `header` section
+			header = [[
+                ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+                ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+                ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+                ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+                ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+                ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
 			sections = {
 				{ section = "header" },
 				{ section = "keys", gap = 1, padding = 1 },
@@ -104,6 +70,10 @@ return {
 		indent = { enabled = true },
 		input = { enabled = true },
 		picker = { enabled = false },
+		bufdelete = {
+			enabled = true,
+			config = function() end,
+		},
 		notifier = {
 			enabled = true,
 			config = function()
