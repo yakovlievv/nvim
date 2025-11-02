812589629124d23b40f57a6fc5cc8f1850e9d72e
yakovlievv <yakovlievv25@gmail.com>
2025-11-02 02:41:44 +0200

    lualine update
    now you can see stuff like macros and many more

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,22 +3,43 @@ return {
 	event = "VimEnter",
 	dependencies = { "nvim-tree/nvim-web-devicons", "catppuccin" },
 	config = function()
-		local lazy_status = require("lazy.status") -- lazy provides this
-
-		local lazy_updates = {
-			function()
-				if lazy_status.has_updates() then
-					return lazy_status.updates()
-				end
-				return ""
-			end,
-			cond = lazy_status.has_updates, -- only show when updates exist
-			color = { fg = "#ff9e64" }, -- optional color
-		}
-
+		Snacks = require("snacks")
 		require("lualine").setup({
 			sections = {
-				lualine_x = { lazy_updates, "encoding", "filetype" },
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = {
+					"searchcount",
+					Snacks.profiler.status(),
+                    -- stylua: ignore
+                    {
+                        function() return require("noice").api.status.command.get() end,
+                        cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+                        color = function() return { fg = Snacks.util.color("Statement") } end,
+                    },
+                    -- stylua: ignore
+                    {
+                        function() return require("noice").api.status.mode.get() end,
+                        cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+                        color = function() return { fg = "#94e2d5" } end,
+                    },
+                    -- stylua: ignore
+                    {
+                        function() return "  " .. require("dap").status() end,
+                        cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+                        color = function() return { fg = Snacks.util.color("Debug") } end,
+                    },
+					{
+						require("lazy.status").updates,
+						cond = require("lazy.status").has_updates,
+						color = function()
+							return { fg = "#fab387" }
+						end,
+					},
+				},
+				lualine_y = { "progress", "location" },
+				lualine_z = { "lsp_status" },
 			},
 		})
 	end,
