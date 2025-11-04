f5ca8df2194853c485578b5738430dcc312f425f
yakovlievv <yakovlievv25@gmail.com>
2025-11-04 10:37:33 +0200

    render-md lazyloading

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,12 +1,11 @@
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    opts = {
-
-    },
-    config = function()
-        require('render-markdown').setup({
-            completions = { lsp = { enabled = true } },
-        })
-    end
+	"MeanderingProgrammer/render-markdown.nvim",
+	ft = "markdown",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+	opts = {},
+	config = function()
+		require("render-markdown").setup({
+			completions = { lsp = { enabled = true } },
+		})
+	end,
 }
