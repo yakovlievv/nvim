6f7094744d81904d76f27916e85b7a6172368409
yakovlievv <yakovlievv25@gmail.com>
2025-11-17 10:30:40 +0200

    stuff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -32,8 +32,8 @@ set("n", "<leader>S", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 --------------------------
 -------- Motions ---------
 --------------------------
-set({ "n", "x", "o" }, ":", ";")
-set({ "n", "x", "o" }, ";", ":")
+-- set({ "n", "x", "o" }, ":", ";")
+-- set({ "n", "x", "o" }, ";", ":")
 
 -- Do J but keep the curson in place
 set("n", "J", "mzJ`z")
@@ -59,7 +59,7 @@ set("n", "<M-Right>", ":vertical resize +2<CR>", { silent = true, desc = "Increa
 
 -- Toggle wrap
 set("n", "<leader>W", function()
-    vim.opt.wrap = not vim.opt.wrap:get()
+	vim.opt.wrap = not vim.opt.wrap:get()
 end, { desc = "Toggle wrap" })
 
 -- toggle floating terminal
@@ -80,13 +80,13 @@ set({ "n", "v", "i" }, "<C-s>", "<Cmd>w<Cr>")
 set({ "n", "v", "i" }, "<C-q>", "<Cmd>qa<Cr>")
 
 set("n", "<leader>bd", function()
-    require("snacks.bufdelete").delete()
+	require("snacks.bufdelete").delete()
 end)
 
 set("n", "<leader>ba", function()
-    require("snacks.bufdelete").all()
+	require("snacks.bufdelete").all()
 end)
 
 set("n", "<leader>bo", function()
-    require("snacks.bufdelete").other()
+	require("snacks.bufdelete").other()
 end)
