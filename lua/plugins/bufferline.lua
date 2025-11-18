e0f65bdc8aead569afe62898eef6e8a4db51b4b2
yakovlievv <yakovlievv25@gmail.com>
2025-11-18 23:53:39 +0200

    bufferline cool settings

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,7 +3,40 @@ return {
 	event = "BufAdd",
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
-
+	keys = {
+		{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+		{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+		{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+		{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+		{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+		{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+		{ "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+		{ "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+		{ "<C-b>h", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+		{ "<C-b>l", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+	},
+	opts = {
+		options = {
+			close_command = function(n)
+				Snacks.bufdelete(n)
+			end,
+			right_mouse_command = function(n)
+				Snacks.bufdelete(n)
+			end,
+			diagnostics = "nvim_lsp",
+			offsets = {
+				{
+					filetype = "neo-tree",
+					text = "Neo-tree",
+					highlight = "Directory",
+					text_align = "left",
+				},
+				{
+					filetype = "snacks_layout_box",
+				},
+			},
+		},
+	},
 	config = function()
 		require("bufferline").setup({
 			options = {
