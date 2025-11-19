ce5d8c1f3263e6d5bc98a2c5533a6ae4f41840d3
yakovlievv <yakovlievv25@gmail.com>
2025-11-19 16:09:23 +0200

    working treesitter new main rewrite

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,19 +5,43 @@ return {
 	version = false,
 	cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
 	event = { "VeryLazy" },
-	opts = {
-		ensure_installed = {
-			"lua",
-			"jsx",
-			"tsx",
-			"javascript",
-		},
-	},
 	config = function()
 		local ts = require("nvim-treesitter")
 		local augroup = vim.api.nvim_create_augroup("my.treesitter", { clear = true })
+		local ensure_installed = {
+			"bash",
+			"c",
+			"cpp",
+			"lua",
+			"python",
+			"javascript",
+			"typescript",
+			"jsx",
+			"tsx",
+			"html",
+			"css",
+			"json",
+			"markdown",
+			"yaml",
+			"gitcommit",
+			"vim",
+		}
 
 		ts.setup()
+
+		for _, parser in ipairs(ensure_installed) do
+			local is_installed, _ = vim.treesitter.language.add(parser)
+			if not is_installed then
+				local available_langs = ts.get_available()
+				local is_available = vim.tbl_contains(available_langs, parser)
+				if is_available then
+					vim.notify("Installing treesitter parser for: " .. parser, vim.log.levels.INFO)
+					ts.install({ parser }):wait(30 * 1000)
+				else
+					vim.notify("The parser doesn't exist: " .. parser, vim.log.levels.INFO)
+				end
+			end
+		end
 
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = "*",
@@ -25,17 +49,27 @@ return {
 			callback = function(event)
 				local filetype = event.match
 				local lang = vim.treesitter.language.get_lang(filetype)
-				local is_installed, error = vim.treesitter.language.add(lang)
+				local is_installed, _ = vim.treesitter.language.add(lang)
 
 				if not is_installed then
-					vim.notify("Installing treesitter parser for " .. lang, vim.log.levels.INFO)
+					local available_langs = ts.get_available()
+					local is_available = vim.tbl_contains(available_langs, lang)
+
+					if is_available then
+						vim.notify("Installing treesitter parser for " .. lang, vim.log.levels.INFO)
+						ts.install({ lang }):wait(30 * 1000)
+					end
 				end
-				pcall(vim.treesitter.start)
-				-- Enable folds
-				vim.wo.foldmethod = "expr"
-				vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-				-- Enable indent
-				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+				if is_installed then
+					local ok, _ = pcall(vim.treesitter.start, event.buf, lang)
+					if not ok then
+						vim.notify("Treesitter fucked up!" .. lang, vim.log.levels.INFO)
+					end
+					vim.wo.foldmethod = "expr"
+					vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					vim.bo[event.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
 			end,
 		})
 	end,
