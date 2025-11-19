86e2bfd1e3d42dbf708f2bf113d2d990be3e2c2d
yakovlievv <yakovlievv25@gmail.com>
2025-11-19 22:45:26 +0200

    completely fucked up treesitter
    i'm here for like 2.5 hrs now and i'm high

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -26,6 +26,21 @@ return {
 			"vim",
 		}
 		local available_parsers = ts.get_available()
+		local installed = ts.get_installed()
+		local not_installed = {}
+
+		-- Get the list of not installed parsers
+		for _, parser in ipairs(ensure_installed) do
+			if not vim.tbl_contains(installed, parser) then
+				table.insert(not_installed, parser)
+			end
+		end
+
+		if #not_installed > 0 then
+			vim.schedule(function()
+				ts.install(not_installed, { summary = true })
+			end)
+		end
 
 		-- Installation proccess of a parser
 		local function parser_sync(parser)
@@ -33,9 +48,9 @@ return {
 			if not is_installed then
 				local is_available = vim.tbl_contains(available_parsers, parser)
 				if is_available then
-					vim.notify("TS installing: " .. parser, vim.log.levels.INFO)
+					-- vim.notify("TS installing: " .. parser, vim.log.levels.INFO)
 					ts.install({ parser }):wait(30 * 1000)
-					vim.notify("Installed " .. parser, vim.log.levels.INFO)
+					-- vim.notify("Installed " .. parser, vim.log.levels.INFO)
 					return true
 				end
 			end
@@ -50,25 +65,25 @@ return {
 		-- end
 
 		local augroup = vim.api.nvim_create_augroup("my.treesitter", { clear = true })
-		-- vim.api.nvim_create_autocmd("FileType", {
-		-- 	pattern = "*",
-		-- 	group = augroup,
-		-- 	callback = function(event)
-		-- 		local lang = vim.treesitter.language.get_lang(event.match)
-		-- 		local is_installed = parser_sync(lang)
-		-- 		if is_installed then
-		-- 			local ok, _ = pcall(vim.treesitter.start, event.buf, lang)
-		--
-		-- 			if not ok then
-		-- 				vim.notify("Treesitter fucked up!" .. lang, vim.log.levels.INFO)
-		-- 				return
-		-- 			end
-		--
-		-- 			vim.wo.foldmethod = "expr"
-		-- 			vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-		-- 			vim.bo[event.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-		-- 		end
-		-- 	end,
-		-- })
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "*",
+			group = augroup,
+			callback = function(event)
+				local lang = vim.treesitter.language.get_lang(event.match)
+				local is_installed, _ = vim.treesitter.language.add(lang)
+				if is_installed then
+					local ok, _ = pcall(vim.treesitter.start, event.buf, lang)
+
+					if not ok then
+						vim.notify("Treesitter fucked up!" .. lang, vim.log.levels.INFO)
+						return
+					end
+
+					vim.wo.foldmethod = "expr"
+					vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					vim.bo[event.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
+			end,
+		})
 	end,
 }
