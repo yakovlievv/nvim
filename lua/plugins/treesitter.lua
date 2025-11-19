d5b6ccf9fddd333630cfe7922066fb4053761cd4
yakovlievv <yakovlievv25@gmail.com>
2025-11-19 09:11:31 +0200

    getting ready new update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,46 +6,55 @@ return {
 	cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
 	event = { "VeryLazy" },
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"lua",
-				"vim",
-				"python",
-				"javascript",
-				"go",
-				"rust",
-				"bash",
-				"c",
-				"diff",
-				"html",
-				"json",
-				"jsonc",
-				"markdown_inline",
-				"markdown",
-				"typescript",
-				"markdown",
-				"python",
-				"query",
-				"regex",
-				"tsx",
-				"vimdoc",
-				"yaml",
-			},
-			auto_install = true,
-			sync_install = false,
-			highlight = { enable = true, additional_vim_regex_highlighting = false },
-			indent = { enable = true },
-			folding = { enable = true },
-			autotag = { enable = false },
-		})
+		-- require("nvim-treesitter.configs").setup({
+		-- ensure_installed = {
+		--     "lua",
+		--     "vim",
+		--     "python",
+		--     "javascript",
+		--     "go",
+		--     "rust",
+		--     "bash",
+		--     "c",
+		--     "diff",
+		--     "html",
+		--     "json",
+		--     "jsonc",
+		--     "markdown_inline",
+		--     "markdown",
+		--     "typescript",
+		--     "markdown",
+		--     "python",
+		--     "query",
+		--     "regex",
+		--     "tsx",
+		--     "vimdoc",
+		--     "yaml",
+		-- },
+		-- auto_install = true,
+		-- sync_install = false,
+		-- highlight = { enable = true, additional_vim_regex_highlighting = false },
+		-- indent = { enable = true },
+		-- folding = { enable = true },
+		-- autotag = { enable = false },
+		-- })
 
+		require("nvim-treesitter").setup()
 		-- Optional: Set up folding by default in all buffers
-		vim.api.nvim_create_autocmd("BufEnter", {
+		vim.api.nvim_create_autocmd("FileType", {
 			pattern = "*",
 			callback = function()
+				-- Enable highlighting
+				pcall(vim.treesitter.start)
+
+				-- Enable folds
 				vim.wo.foldmethod = "expr"
 				vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+				-- Enable indent
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 			end,
 		})
+		print(vim.inspect(require("nvim-treesitter").get_installed()))
 	end,
 }
