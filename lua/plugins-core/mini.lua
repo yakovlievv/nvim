ed32d9f4d890364e44e8da979d75ea62853742b0
yakovlievv <yakovlievv25@gmail.com>
2025-12-11 15:47:04 +0200

    mini icons

--- lua/plugins-core/mini.lua
+++ lua/plugins-core/mini.lua
@@ -1,33 +1,40 @@
 return {
-    {
-        "nvim-mini/mini.surround",
-        version = "*",
-        event = "VeryLazy",
-        config = function()
-            local surround = require('mini.surround')
-            surround.setup({
-                mappings = {
-                    add = '<A-s>a',       -- Add surrounding in Normal and Visual modes
-                    delete = '<A-s>d',    -- Delete surrounding
-                    find = '<A-s>f',      -- Find surrounding (to the right)
-                    find_left = '<A-s>F', -- Find surrounding (to the left)
-                    highlight = '<A-s>h', -- Highlight surrounding
-                    replace = '<A-s>r',   -- Replace surrounding
+	{
+		"nvim-mini/mini.surround",
+		version = "*",
+		event = "VeryLazy",
+		config = function()
+			local surround = require("mini.surround")
+			surround.setup({
+				mappings = {
+					add = "<A-s>a", -- Add surrounding in Normal and Visual modes
+					delete = "<A-s>d", -- Delete surrounding
+					find = "<A-s>f", -- Find surrounding (to the right)
+					find_left = "<A-s>F", -- Find surrounding (to the left)
+					highlight = "<A-s>h", -- Highlight surrounding
+					replace = "<A-s>r", -- Replace surrounding
 
-
-                    suffix_last = 'h', -- Suffix to search with "prev" method
-                    suffix_next = 'l', -- Suffix to search with "next" method
-                },
-            })
-            vim.keymap.del('n', 'sn')
-        end,
-    },
-    {
-        "nvim-mini/mini.ai",
-        version = "*",
-        event = "VeryLazy",
-        config = function()
-            require("mini.ai").setup()
-        end,
-    }
+					suffix_last = "h", -- Suffix to search with "prev" method
+					suffix_next = "l", -- Suffix to search with "next" method
+				},
+			})
+			vim.keymap.del("n", "sn")
+		end,
+	},
+	{
+		"nvim-mini/mini.ai",
+		version = "*",
+		event = "VeryLazy",
+		config = function()
+			require("mini.ai").setup()
+		end,
+	},
+	{
+		"nvim-mini/mini.icons",
+		version = false,
+		config = function()
+			require("mini.icons").setup({})
+			require("mini.icons").mock_nvim_web_devicons()
+		end,
+	},
 }
--- lua/plugins-full/neotree.lua
+++ lua/plugins-full/neotree.lua
@@ -12,7 +12,6 @@ return {
 	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
 
