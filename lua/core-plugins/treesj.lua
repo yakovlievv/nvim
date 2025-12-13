c4f5b83f2acc9c57c04fb60d6e1b96b61d7b1598
yakovlievv <yakovlievv25@gmail.com>
2025-12-14 00:35:30 +0200

    treesj new keymaos

--- lua/core-plugins/treesj.lua
+++ lua/core-plugins/treesj.lua
@@ -1,7 +1,29 @@
 return {
 	"Wansmer/treesj",
-	keys = { "<leader>m", "<leader>j", "<leader>s" },
-	dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
+	keys = {
+		{
+			"<leader>jm",
+			function()
+				require("treesj").toggle()
+			end,
+			desc = "TreeSJ toggle",
+		},
+		{
+			"<leader>jj",
+			function()
+				require("treesj").join()
+			end,
+			desc = "TreeSJ join",
+		},
+		{
+			"<leader>js",
+			function()
+				require("treesj").split()
+			end,
+			desc = "TreeSJ split",
+		},
+	},
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
 	config = function()
 		require("treesj").setup()
 	end,
