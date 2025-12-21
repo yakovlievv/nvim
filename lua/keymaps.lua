6a9804d3707774fb275e06a9621e405363a235f9
yakovlievv <yakovlievv25@gmail.com>
2025-12-21 23:30:07 +0200

    keymaps update

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,23 +1,28 @@
-local set = vim.keymap.set
-local opts = { silent = true, noremap = true }
+-- wrapper function
+local function map(modes, key, action, opts)
+	local default_opts = { silent = true, noremap = true }
+	-- merge tables, user opts override defaults
+	local final_opts = vim.tbl_extend("force", default_opts, opts or {})
+	vim.keymap.set(modes, key, action, final_opts)
+end
 
 -- better up and down
-set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, desc = "Down" })
-set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, desc = "Up" })
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, desc = "Down" })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, desc = "Up" })
 
 -- set("n", "+", "<C-a>")
 -- set("n", "-", "<C-x>")
 
 -- better n, N
-set("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
-set("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
-set("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
-set("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
-set("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
-set("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
+map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
+map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
 
-set("n", "<leader>K", "<cmd>norm! K<cr>", { desc = "Keywordprg" })
-set("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
+map("n", "<leader>K", "<cmd>norm! K<cr>", { desc = "Keywordprg" })
+map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
 
 -- Jump so specific diagnostic of certain severity
 local diagnostic_goto = function(next, severity)
@@ -30,118 +35,118 @@ local diagnostic_goto = function(next, severity)
 	end
 end
 
-set("i", "<C-h>", "<Left>", { desc = "move left" })
-set("i", "<C-l>", "<Right>", { desc = "move right" })
-set("i", "<C-j>", "<Down>", { desc = "move down" })
-set("i", "<C-k>", "<Up>", { desc = "move up" })
-set("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
-set("i", "<C-e>", "<ESC>$a", { desc = "move end of line" })
-set("n", "<C-c>", "<cmd>%y+<CR>", { desc = "general copy whole file" })
+map("i", "<C-h>", "<Left>", { desc = "move left" })
+map("i", "<C-l>", "<Right>", { desc = "move right" })
+map("i", "<C-j>", "<Down>", { desc = "move down" })
+map("i", "<C-k>", "<Up>", { desc = "move up" })
+map("i", "<C-b>", "<cmd>norm b<Cr>", { desc = "move beginning of line" })
+map("i", "<C-w>", "<cmd>norm w<Cr>", { desc = "w in inser mode" })
+map("i", "<C-e>", "<cmd>norm e<Cr>", { desc = "e in inser mode" })
+map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "general copy whole file" })
 
-set("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
-set("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
-set("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
-set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
-set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
-set("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
+map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
 
-set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
 
-set("n", "[q", vim.cmd.cprev, { desc = "Previous Quickfix" })
-set("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix" })
+map("n", "[q", vim.cmd.cprev, { desc = "Previous Quickfix" })
+map("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix" })
 
-set("n", "<C-k>", vim.cmd.cprev, { desc = "Previous Quickfix" })
-set("n", "<C-j>", vim.cmd.cnext, { desc = "Next Quickfix" })
+map("n", "<C-k>", vim.cmd.cprev, { desc = "Previous Quickfix" })
+map("n", "<C-j>", vim.cmd.cnext, { desc = "Next Quickfix" })
 
 -- Move selected lines
-set("x", "K", ":m '<-2<cr>gv=gv", { silent = true })
-set("x", "J", ":m '>+1<CR>gv=gv", { silent = true })
+map("x", "K", ":m '<-2<cr>gv=gv")
+map("x", "J", ":m '>+1<CR>gv=gv")
 
 -- Indent witout reselecting everytime
-set("x", ">", ">gv")
-set("x", "<", "<gv")
+map("x", ">", ">gv")
+map("x", "<", "<gv")
 
 -- Not use system clipboard
-set({ "x", "n", "o" }, "<leader>p", [["_dhp]])
-set({ "x", "n", "o" }, "<leader>P", [["_dhP]])
-set({ "x", "n", "o" }, "<leader>c", [["_c]])
-set({ "x", "n", "o" }, "<leader>C", [["_C]])
-set({ "x", "n", "o" }, "<leader>d", [["_d]])
-set({ "x", "n", "o" }, "<leader>D", [["_D]])
+map({ "x", "n", "o" }, "<leader>p", [["_dhp]])
+map({ "x", "n", "o" }, "<leader>P", [["_dhP]])
+map({ "x", "n", "o" }, "<leader>c", [["_c]])
+map({ "x", "n", "o" }, "<leader>C", [["_C]])
+map({ "x", "n", "o" }, "<leader>d", [["_d]])
+map({ "x", "n", "o" }, "<leader>D", [["_D]])
 
 -- Change the word under the word
-set("n", "<leader>S", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-set("v", "<leader>S", [[y:%s/<C-r>"/<C-r>"/gI<Left><Left><Left>]])
+map("n", "<leader>S", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+map("v", "<leader>S", [[y:%s/<C-r>"/<C-r>"/gI<Left><Left><Left>]])
 
 -- set({ "n", "x", "o" }, ":", ";")
 -- set({ "n", "x", "o" }, ";", ":")
 
 -- Do J but keep the curson in place
-set("n", "J", function()
+map("n", "J", function()
 	local pos = vim.api.nvim_win_get_cursor(0)
 	vim.cmd("normal! J")
 	vim.api.nvim_win_set_cursor(0, pos)
 end, { desc = "Join lines and stay in place" })
 
 -- Call lazy
-set("n", "<leader>l", "<cmd>Lazy<CR>")
+map("n", "<leader>l", "<cmd>Lazy<CR>")
 
 -- Reindent entire buffer while keeping cursor
-vim.keymap.set("n", "<leader>=", function()
+map("n", "<leader>=", function()
 	local view = vim.fn.winsaveview()
 	vim.cmd("normal! gg=G")
 	vim.fn.winrestview(view)
 end, { desc = "Reindent whole file and keep cursor" })
 
 -- chmod a file (make it executable)
-set("n", "<leader>x", ":!chmod +x %<CR>", { silent = false })
+map("n", "<leader>x", ":!chmod +x %<CR>", { silent = false })
 
 -- resize a window
-set("n", "<M-Up>", ":resize -2<CR>", { silent = true, desc = "Decrease height" })
-set("n", "<M-Down>", ":resize +2<CR>", { silent = true, desc = "Increase height" })
-set("n", "<M-Left>", ":vertical resize -2<CR>", { silent = true, desc = "Decrease width" })
-set("n", "<M-Right>", ":vertical resize +2<CR>", { silent = true, desc = "Increase width" })
+map("n", "<M-Up>", ":resize -2<CR>", { desc = "Decrease height" })
+map("n", "<M-Down>", ":resize +2<CR>", { desc = "Increase height" })
+map("n", "<M-Left>", ":vertical resize -2<CR>", { desc = "Decrease width" })
+map("n", "<M-Right>", ":vertical resize +2<CR>", { desc = "Increase width" })
 
 -- Toggle wrap
-set("n", "<leader>W", function()
+map("n", "<leader>W", function()
 	vim.opt.wrap = not vim.opt.wrap:get()
 end, { desc = "Toggle wrap" })
 
-set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+map("n", "<leader>rn", vim.lsp.buf.rename)
+map("n", "<leader>ca", vim.lsp.buf.code_action)
 
 -- switch to latest buffer
-set("n", "<leader>bb", ":buffer #<CR>", opts)
-set("n", "<C-b><C-b>", ":buffer #<CR>", opts)
+map("n", "<leader>bb", ":buffer #<CR>")
+map("n", "<C-b><C-b>", ":buffer #<CR>", opts)
 
 -- saving and quiting
 -- set({ "n", "v" }, "<leader>w", "<cmd>w<Cr>")
 -- set({ "n", "v" }, "<leader>q", "<cmd>q<Cr>")
 -- set({ "n", "v" }, "<leader>Q", "<cmd>qa<Cr>")
 --
-set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
-set({ "n", "v", "i" }, "<C-q>", "<Cmd>qa<Cr>")
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+map({ "n", "v", "i" }, "<C-q>", "<Cmd>qa<Cr>")
 
 -- these just make sense don't they?
-set({ "n", "x", "o" }, "<S-l>", "g_")
-set({ "n", "x", "o" }, "<S-h>", "_")
+map({ "n", "x", "o" }, "<S-l>", "g_")
+map({ "n", "x", "o" }, "<S-h>", "_")
 
 -- Close floating windows in insert mode with K
-set(
+map(
 	"i",
 	"<A-k>",
-	'<Cmd>lua for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do if vim.api.nvim_win_is_valid(win) then local cfg = vim.api.nvim_win_get_config(win) if cfg.relative ~= "" then vim.api.nvim_win_close(win, true) end end end <CR>',
-	{ noremap = true, silent = true }
+	'<Cmd>lua for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do if vim.api.nvim_win_is_valid(win) then local cfg = vim.api.nvim_win_get_config(win) if cfg.relative ~= "" then vim.api.nvim_win_close(win, true) end end end <CR>'
 )
 
 -- Close only floating windows safely in insert mode
 -- Utilities
 
-set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-set("i", "<C-p>", "<Nop>", { noremap = true, silent = true })
+map("n", "<Esc>", "<cmd>nohlsearch<CR>")
+map("i", "<C-p>", "<Nop>")
 
 -- uninstall treesitter parsers
-set(
+map(
 	"n",
 	"<leader>tu",
 	[[:lua require("nvim-treesitter").uninstall(require("nvim-treesitter").get_installed(), { summary = true })]]
