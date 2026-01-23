ba54c1d69206d8309f38d672cd031236cc344029
yakovlievv <yakovlievv25@gmail.com>
2026-01-23 03:13:22 +0200

    file navigation update

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -5,38 +5,102 @@ return {
 		menu = {
 			width = vim.api.nvim_win_get_width(0) - 4,
 		},
-		settings = {
-			save_on_toggle = true,
-		},
+		-- settings = {
+		-- 	save_on_toggle = true,
+		-- },
 	},
-	keys = function()
-		local keys = {
-			{
-				"<leader>H",
-				function()
-					require("harpoon"):list():add()
-				end,
-				desc = "Harpoon File",
-			},
-			{
-				"<leader>h",
-				function()
-					local harpoon = require("harpoon")
-					harpoon.ui:toggle_quick_menu(harpoon:list())
-				end,
-				desc = "Harpoon Quick Menu",
-			},
-		}
+	config = function()
+		local harpoon = require("harpoon")
 
-		for i = 1, 9 do
-			table.insert(keys, {
-				"<leader>" .. i,
-				function()
-					require("harpoon"):list():select(i)
-				end,
-				desc = "Harpoon to File " .. i,
-			})
-		end
-		return keys
+		-- REQUIRED
+		harpoon:setup()
+		-- REQUIRED
+
+		vim.keymap.set("n", "<leader>h", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<leader>H", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+
+		vim.keymap.set("n", "<C-h>", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<C-j>", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<C-k>", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<C-l>", function()
+			harpoon:list():select(4)
+		end)
+
+		-- Toggle previous & next buffers stored within Harpoon list
+		vim.keymap.set("n", "<C-S-P>", function()
+			harpoon:list():prev()
+		end)
+		vim.keymap.set("n", "<C-S-N>", function()
+			harpoon:list():next()
+		end)
 	end,
+	-- keys = function()
+	-- 	local keys = {
+	-- 		{
+	-- 			"<leader>h",
+	-- 			function()
+	-- 				require("harpoon"):list():add()
+	-- 			end,
+	-- 			desc = "Harpoon File",
+	-- 		},
+	-- 		{
+	-- 			"<leader>H",
+	-- 			function()
+	-- 				local harpoon = require("harpoon")
+	-- 				harpoon.ui:toggle_quick_menu(harpoon:list())
+	-- 			end,
+	-- 			desc = "Harpoon Quick Menu",
+	-- 		},
+	-- 		{
+	-- 			"<C-h>",
+	-- 			function()
+	-- 				require("harpoon").list():select(1)
+	-- 			end,
+	-- 			desc = "Harpoon Quick Menu",
+	-- 		},
+	--
+	-- 		{
+	-- 			"<C-j>",
+	-- 			function()
+	-- 				require("harpoon").list():select(2)
+	-- 			end,
+	-- 			desc = "Harpoon Quick Menu",
+	-- 		},
+	--
+	-- 		{
+	-- 			"<C-k>",
+	-- 			function()
+	-- 				require("harpoon").list():select(3)
+	-- 			end,
+	-- 			desc = "Harpoon Quick Menu",
+	-- 		},
+	-- 		{
+	-- 			"<C-l>",
+	-- 			function()
+	-- 				require("harpoon").list():select(4)
+	-- 			end,
+	-- 			desc = "Harpoon Quick Menu",
+	-- 		},
+	-- 	}
+	--
+	-- 	-- for i = 1, 9 do
+	-- 	-- 	table.insert(keys, {
+	-- 	-- 		"<leader>" .. i,
+	-- 	-- 		function()
+	-- 	-- 			require("harpoon"):list():select(i)
+	-- 	-- 		end,
+	-- 	-- 		desc = "Harpoon to File " .. i,
+	-- 	-- 	})
+	-- 	-- end
+	-- 	return keys
 }
