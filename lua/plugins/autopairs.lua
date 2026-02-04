f7d86c395d40fc2ce82eb4b15de64455eb511161
yakovlievv <yakovlievv25@gmail.com>
2026-02-04 17:28:19 +0200

    autopairs insert (  )

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,5 +1,21 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-	config = true,
+	config = function()
+		require("nvim-autopairs").setup({
+			check_ts = true, -- use treesitter
+			enable_check_bracket_line = false,
+		})
+
+		-- Add spaces inside pairs when you hit space
+		local Rule = require("nvim-autopairs.rule")
+		local npairs = require("nvim-autopairs")
+
+		npairs.add_rules({
+			Rule(" ", " "):with_pair(function(opts)
+				local pair = opts.line:sub(opts.col - 1, opts.col)
+				return vim.tbl_contains({ "()", "[]", "{}" }, pair)
+			end),
+		})
+	end,
 }
