f5bdfe59fcbebe6f4ce28936339cfd7bc3f15071
yakovlievv <yakovlievv25@gmail.com>
2026-06-11 15:37:05 +0300

    smart goto definition
    gd will invoke vim.lsp.buf.definition() if there is one deninition but
    if there is multiple it'll use snacke picker for browsing multiple
    definitions

--- lua/plugins/snacks/keys/lsp.lua
+++ lua/plugins/snacks/keys/lsp.lua
@@ -1,6 +1,22 @@
+local function goto_definition()
+	vim.lsp.buf.definition({
+		on_list = function(opts)
+			local items = opts.items or {}
+			if #items > 1 then
+				-- multiple definitions: hand off to the picker
+				Snacks.picker.lsp_definitions()
+				return
+			end
+			-- single (or zero) definition: jump directly
+			vim.fn.setqflist({}, " ", opts)
+			vim.cmd.cfirst()
+		end,
+	})
+end
+
 return {
     --stylua: ignore start
-    -- { "gd",         function() Snacks.picker.lsp_definitions() end,       desc = "Goto Definition" },
+    { "gd",         goto_definition,                                      desc = "Goto Definition" },
     { "gD",         function() Snacks.picker.lsp_declarations() end,      desc = "Goto Declaration" },
     { "gr",         function() Snacks.picker.lsp_references() end,        nowait = true,                desc = "References" },
     { "gI",         function() Snacks.picker.lsp_implementations() end,   desc = "Goto Implementation" },
